b91e3ec97ba45551d0fe402be80f17eb4df4c520
Imon <contact@imonweb.co.uk>
2024-12-08 11:11:52 +0000

    slider

--- header.php
+++ header.php
@@ -19,3 +19,4 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+
--- index.php
+++ index.php
@@ -3,6 +3,9 @@
  // silence is golden...
 get_header();?>
 
+<?php get_template_part('template-parts/nav'); ?>
+<?php get_template_part('template-parts/slider'); ?>
+
 <?php 
   if(have_posts()) :
     while(have_posts()) :
@@ -10,6 +13,7 @@ get_header();?>
       ?>
         <h1><?php the_title(); ?></h1>
         <p><?php the_excerpt(); ?></p>
+        <a href="<?php the_permalink( ); ?>">read more</a>
         <hr>
       <?php
     endwhile;
